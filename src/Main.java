f861f970917d494ce0326adae52ffdf68b2f4933
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-14 21:47:30 +0300

    MethodName_(Методы)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.time.LocalDate;
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Методы!");
         //Задание 1
         System.out.println("Задание 1");
         printLeapNotLapYear(2020);
+        printVersionOS()
     }
 
         public static boolean isLeapYear (int year){
@@ -16,5 +20,21 @@ public class Main {
                 return;
             }
             System.out.println(year + " не високосный год");
+        }
+
+        public static boolean isNowYear(int year){
+        return year == ;
+        }
+        public static String getVersionOS (int versionOS){
+        if (versionOS == 0){
+            return "iOS";
+        }
+        return "Android";
+        }
+
+        public static void printVersionOS(int year, int versionOS) {
+        if (isNowYear(year)){
+            System.out.println();
+        }
         }
 }
