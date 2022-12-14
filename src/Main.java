e08597e4ac03cf0a0163321adaa4aea360335ebb
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-14 21:12:23 +0300

    MethodName_(Методы)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Методы!");
+        printLeapNotLapYear(2020);
     }
 
 
@@ -15,6 +16,8 @@ public class Main {
         public static void printLeapNotLapYear(int year) {
             if (isLeapYear(year)){
                 System.out.println(year + " високосный год");
+                return;
             }
+
         }
 }
