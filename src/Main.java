81447e01e53ffc85aa14c032ba3f8c8e96128199
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-14 21:06:45 +0300

    MethodName_(Методы)

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Методы!");
     }
+
+
+
+        //Задание 1
+        System.out.println("Задание 1");
+
+        public static boolean isLeapYear (int year){
+            return year % 4 == 0 && year % 100 !=0 || year % 400 ==0;
+        }
+
+
 }
